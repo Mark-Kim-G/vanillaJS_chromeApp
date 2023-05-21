3e0497498d8b9292df097ed861c4d46bf4a8a59b
Helllper <siongjae@msn.com>
2023-05-22 00:50:14 +1200

    Further progression into removing and adding classList for interaction
    Further progression into removing and adding classList for interaction

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName"
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add("hidden");
     const userName = loginInput.value;
+    localStorage.setItem("USERNAME_KEY", userName);
     console.log(userName);
-    greeting.innerText = "Hello " + userName;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(userName);
 }
 
 function handleLinkClick(event){
@@ -18,7 +19,24 @@ function handleLinkClick(event){
 }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
 
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(userName){
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+
+const savedUserName = localStorage.getItem("USERNAME_KEY");
+
+if (savedUserName === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(savedUserName);
+}
  
